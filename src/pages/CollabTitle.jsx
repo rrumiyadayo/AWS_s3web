478dc966986968d1189d4e92f4db59788438bdfc
Azim Sofi <azimmiskandar@gmail.com>
2025-01-04 20:33:55 +0900

    Hyperlink

--- src/pages/CollabTitle.jsx
+++ src/pages/CollabTitle.jsx
@@ -5,10 +5,8 @@ import MotionHoverUp from "../components/MotionHoverUp";
 function CollabTitle() {
   return (
     <MotionHoverUp className="text-4xl font-bold text-center text-gray-800 mb-16">
-      <motion.h1
-        className="text-inherit font-inherit text-center mb-0" // Inherit styles from MotionHoverUp
-      >
-        コラボセクション
+      <motion.h1 className="text-inherit font-inherit text-center mb-0">
+        <div id="コラボ">コラボレーション</div>
       </motion.h1>
     </MotionHoverUp>
   );
