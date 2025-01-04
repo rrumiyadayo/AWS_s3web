509644ac94fb375734853f3b0c229ef942fdabba
Azim Sofi <azimmiskandar@gmail.com>
2025-01-04 20:36:25 +0900

    Styling

--- src/pages/CollabTitle.jsx
+++ src/pages/CollabTitle.jsx
@@ -5,7 +5,10 @@ import MotionHoverUp from "../components/MotionHoverUp";
 function CollabTitle() {
   return (
     <MotionHoverUp className="text-4xl font-bold text-center text-gray-800 mb-16">
-      <motion.h1 className="text-inherit font-inherit text-center mb-0">
+      <motion.h1
+        className="text-inherit font-inherit text-center mb-0"
+        style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)" }}
+      >
         <div id="コラボ">コラボレーション</div>
       </motion.h1>
     </MotionHoverUp>
